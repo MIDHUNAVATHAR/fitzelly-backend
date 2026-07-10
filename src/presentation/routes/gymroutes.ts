62b4ac459690d92b2d727d5a39a3423c26123884
Midhun M <midhun201819@gmail.com>
2026-07-10 09:39:06 +0530

    remove zod validation from login route- testing purpose

--- src/presentation/routes/gymroutes.ts
+++ src/presentation/routes/gymroutes.ts
@@ -31,7 +31,7 @@ const router = Router();
 
 router.post(GYM_ROUTE.INITIATE_SIGNUP, validateRequest(allFieldsMin3Schema), gymAuthenticationController.initiateSignUp.bind(gymAuthenticationController));
 router.post(GYM_ROUTE.COMPLETE_SINGUP, validateRequest(allFieldsMin3Schema), gymAuthenticationController.completeSignUp.bind(gymAuthenticationController))
-router.post(GYM_ROUTE.LOGIN, validateRequest(allFieldsMin3Schema), gymAuthenticationController.login.bind(gymAuthenticationController));
+router.post(GYM_ROUTE.LOGIN,  gymAuthenticationController.login.bind(gymAuthenticationController));
 router.post(GYM_ROUTE.INITIATE_FORGOTPASSWORD, validateRequest(allFieldsMin3Schema), gymAuthenticationController.initiateForgotPassword.bind(gymAuthenticationController))
 router.post(GYM_ROUTE.COMPLETE_FORGOTPASSWORD, validateRequest(allFieldsMin3Schema), gymAuthenticationController.completeForgotPassword.bind(gymAuthenticationController))
 router.post(GYM_ROUTE.RESET_PASSWORD, validateRequest(allFieldsMin3Schema), gymAuthenticationController.resetPassword.bind(gymAuthenticationController));
